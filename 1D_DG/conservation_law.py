f19ade3d9fb1698d81320ee43dc2640348d31d04
Wang Haifeng <HF_Wang1031@163.com>
2021-05-06 13:09:37 +0800

    Add some new Case

--- 1D_DG/conservation_law.py
+++ 1D_DG/conservation_law.py
@@ -49,7 +49,7 @@ class CL_Solver:
 
         #step 1: compute the local stencil
         if self.flux_type == 1:
-            c = 1 # set velocity number
+            c = -4 # set velocity number
             f_l =np.array([basis(-1) for basis in self.basis]).reshape((1,-1))
             f_r = np.array([basis(1) for basis in self.basis]).reshape((1,-1))
             phi_l = np.array([basis(-1) for basis in self.basis]).reshape((-1,1))
@@ -81,19 +81,47 @@ class CL_Solver:
             return lambda basis_weight:np.matmul(SemiMatrix,basis_weight)
 
         elif self.flux_type == 2:
+
             def operator(weights):
                 weights = np.reshape(weights,(self.K,self.N))
-                local_func = lambda x,i:sum([weights[i][j]*self.basis[i](x) for j in range(self.N)])
-                #TODO
-                #1.numerical flux
-
-                #2.RHS_interger 
-
-                raise
-
+                local_func = lambda x,i:sum([weights[i][j]*self.basis[j](x) for j in range(self.N)])
+                f_u = lambda x,i:(local_func(x,i))**2/2
+                df_u = lambda x,i:local_func(x,i)
+                result = np.zeros((self.N,self.K))
+                for e in range(self.K):
+                    for i in range(self.N):
+                        #1.numerical flux
+                        num_flux_r = 1/2*(f_u(1,e) + f_u(-1,e+1 if e<self.K-1 else 0) \
+                                   - np.max([np.abs(df_u(-1,e)),np.abs(df_u(1,e+1 if e<self.K-1 else 0))])*(local_func(-1,e+1 if e<self.K-1 else 0) - local_func(1,e)))*self.basis[i](1)
+                        num_flux_l = 1/2*(f_u(1,e-1) + f_u(-1,e) \
+                                   - np.max([np.abs(df_u(1,e-1)),np.abs(df_u(-1,e))])*(local_func(-1,e) - local_func(1,e-1)))*self.basis[i](-1)
+                        #2.RHS_interger
+                        rhs_interger = integrate.quad(lambda x:f_u(x,e)*self.Dbasis[i](x),-1,1)[0]
+                        result[i][e] = rhs_interger + num_flux_l - num_flux_r
+                    result[:,e] = np.linalg.solve(self.mass_matrix,result[:,e])/(self.delta_x[e]/2)
+                return np.reshape(result,(-1,1),"F")
             return operator
+
         elif self.flux_type == 3:
-            pass
+            def operator(weights):
+                weights = np.reshape(weights,(self.K,self.N))
+                local_func = lambda x,i:sum([weights[i][j]*self.basis[j](x) for j in range(self.N)])
+                f_u = lambda x,i:(local_func(x,i))**2/(local_func(x,i)**2 + 0.5*(1-local_func(x,i))**2)
+                df_u = lambda x,i:local_func(x,i)*(4/9)*(1-local_func(x,i))/(local_func(x,i)**2 -2/3*local_func(x,i) + 1/3*local_func(x,i))**2
+                result = np.zeros((self.N,self.K))
+                for e in range(self.K):
+                    for i in range(self.N):
+                        #1.numerical flux
+                        num_flux_r = 1/2*(f_u(1,e) + f_u(-1,e+1 if e<self.K-1 else 0) \
+                                   - 2*(local_func(-1,e+1 if e<self.K-1 else 0) - local_func(1,e)))*self.basis[i](1)
+                        num_flux_l = 1/2*(f_u(1,e-1) + f_u(-1,e) \
+                                   - 2*(local_func(-1,e) - local_func(1,e-1)))*self.basis[i](-1)
+                        #2.RHS_interger
+                        rhs_interger = integrate.quad(lambda x:f_u(x,e)*self.Dbasis[i](x),-1,1)[0]
+                        result[i][e] = rhs_interger + num_flux_l - num_flux_r
+                    result[:,e] = np.linalg.solve(self.mass_matrix,result[:,e])/(self.delta_x[e]/2)
+                return np.reshape(result,(-1,1),"F")
+            return operator
         else:
             raise RunTimeError("To be continue!")
         self.SemiMatrix = SemiMatrix
@@ -139,27 +167,63 @@ class CL_Solver:
 
 # initial wave for Linear advection equation
 def sine_wave(x):
-    return np.sin(10*np.pi*x)
+    # x in [0,1]
+    return np.sin(np.pi*x)
 
 def multi_wave(x):
+    # x in [0,1.4]
     if 0.2 < x <=0.3:
         return 10*(x-0.2)
     elif 0.3 < x <= 0.4:
         return 10*(0.4-x)
     elif 0.6< x <=0.8:
-        return 1
+        return np.ones_like(x)
     elif 1< x <=1.2:
         return 100*(x-1)*(1.2-x)
     else:
-        return 0
+        return np.zeros_like(x)
+# initial wave for Burger's equation
+
+def shock_collision(x):
+    # x in [0,1]
+    if x<=0.2:
+        return 10*np.ones_like(x)
+    elif 0.2<x<=0.4:
+        return 6*np.ones_like(x)
+    elif 0.4<x<=0.6:
+        return 0*np.ones_like(x)
+    elif x>0.6:
+        return -4*np.ones_like(x)
+
+
+def compound_wave(x):
+    # x in [-4,4]
+    if x >= 1 or x <= -1:
+        return np.sin(np.pi*x)
+    elif -1<x<= -0.5:
+        return 3*np.ones_like(x)
+    elif -0.5<x<=0:
+        return 1*np.ones_like(x)
+    elif 0<x<= 0.5:
+        return 3*np.ones_like(x)
+    elif 0.5<x<=1:
+        return 2*np.ones_like(x)
+
+# initial wave of Buckley Levertt equation
+def b_l_initial(x):
+    if x>= 0.5:
+        return 0.95*np.ones_like(x)
+    elif x< 0.5:
+        return 0.1*np.ones_like(x)
+
 
 if __name__ == "__main__":
 
-    solver = CL_Solver(flux_type = 1,basis_order=2,space_interval=[0,1.4],ele_num = 10)
+    solver = CL_Solver(flux_type = 2,basis_order=2,space_interval=[0,1],ele_num = 100)
 
     plt.ion()
-    solver.reset(multi_wave)
-    for _ in range(1400):
+    solver.reset(b_l_initial)
+    for _ in range(140):
         solver.draw_step(solver.BasisWeights)
         solver.step(0.001)
         plt.pause(0.001)
